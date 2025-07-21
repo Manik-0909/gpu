9e729adfdfd6a04b4727e275f3bc10423ade29cd
Manik Gupta <manikgupta254@gmail.com>
2025-07-21 12:52:21 +0530

    feedback fixes

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ window.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => loader.remove(), 100);
    }, 1000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+   const closeButton = document.querySelector('[data-bs-dismiss="alert"]');
+   const alertSection = document.querySelector('[role="alert"]');
+
+   if (closeButton && alertSection) {
+      closeButton.addEventListener("click", function () {
+         alertSection.style.display = "none";
+      });
+   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+   const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+   );
+   [...tooltipTriggerList].forEach((tooltipTriggerEl) => {
+      new bootstrap.Tooltip(tooltipTriggerEl);
+   });
+});
